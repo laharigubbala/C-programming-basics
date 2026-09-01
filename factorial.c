43e9de078c479eb62e401b443b4c83b02ecfc3a8
laharigubbala <laharigubbbala@gmail.com>
2026-09-01 19:39:41 +0530

    Add factorial calculation program in C

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial = %llu", factorial);
+
+    return 0;
+}
